a14ef27357041403b87bb02717d67105f6dc234a
CabosManuel <cabos.apolaya@gmail.com>
2023-11-15 15:03:58 -0500

    Add base URLs & function to get trending movies

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log('API_KEY = ' + API_KEY);
+const URL = 'https://api.themoviedb.org/3/';
+const URL_IMG = 'https://image.tmdb.org/t/p/w300';
+
+async function getTrendingMoviesPreview() {
+	const response = await fetch(`${URL}/trending/movie/week?api_key=${API_KEY}`); // &language=en-US
+	const data = await response.json();
+
+	const movies = data.results;
+	console.log(movies);
+
+	movies.forEach(movie => {
+		const trendingMoviesContainer = document.querySelector('#home-view .trending-movies');
+
+		const trendingMovieDiv = document.createElement('div');
+		trendingMovieDiv.classList.add('trending-movie');
+
+		const movieImg = document.createElement('img');
+		movieImg.setAttribute('alt', movie.title);
+		movieImg.setAttribute('src', `${URL_IMG}${movie.poster_path}`);
+
+		trendingMovieDiv.appendChild(movieImg);
+		trendingMoviesContainer.appendChild(trendingMovieDiv);
+	});
+}
+
+getTrendingMoviesPreview();
